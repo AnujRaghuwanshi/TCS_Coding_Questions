e4b74bb7334e3f795952321a4d52ac5eaf4765a2
Anuj <anujraghuwanshipip04@gmail.com>
2026-04-16 21:38:43 +0530

    Fractional knapsack impelmentation

---
+++ Fractional_Knapsack.cpp
@@ -0,0 +1,41 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+bool compare(vector<int>& a, vector<int>& b){
+    double x = (1.0 * a[0]) / a[1];
+    double y = (1.0 * b[0]) / b[1];
+    return x > y;
+}
+
+int fractionalKnapsack(vector<int>& val, vector<int>& wt, int W){
+    int n = val.size();
+    vector<vector<int>> items(n,vector<int>(2));
+
+    for(int i = 0; i < n; i++){
+        items[i][0] = val[i];
+        items[i][1] = wt[i];
+    }
+
+    sort(begin(items), end(items), compare);
+
+    double res = 0.0;
+    for(int i = 0; i < n; i++){
+        if(items[i][1] <= W){
+            res += items[i][0];
+            W -= items[i][1];
+        }else{
+            res += (1.0 * items[i][0] / items[i][1]) * W;
+            break;
+        }
+    }
+    return res;
+}
+
+int main(){
+    vector<int> val = {10,20,30};
+    vector<int> wt = {1,2,3};
+    int capacity = 6;
+
+    cout<<fractionalKnapsack(val,wt,capacity);
+    return 0;
+}
